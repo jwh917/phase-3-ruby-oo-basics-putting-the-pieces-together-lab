186560934e9e749c324c0ed5f0d3a00414021585
Julien Hnery <jwh_917@yahoo.com>
2022-08-24 08:28:46 -0400

    Object Orientation Basics

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,33 @@
+class Book
+  attr_reader :title
+  attr_reader :author
+  attr_reader :page_count
+  attr_reader :genre
+  
+  
+  # setter method
+  def initialize(title)
+    @title = title
+    # @genre = genre
+  end
+
+  def author=(author)
+    @author = author
+  end
+
+  def page_count=(page_count)
+    @page_count = page_count
+  end
+
+  def genre=(genre)
+    @genre = genre
+  end
+
+  def turn_page
+    puts "Flipping the page...wow, you read fast!"
+  end
+
+end
 
 
+# , author, page_count, genre
--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,37 @@
 # Make your shoe class here!
+class Shoe
+  attr_reader :brand
+  attr_reader :color
+  attr_reader :size
+  attr_reader :material
+  attr_reader :condition
+  attr_reader :cobble
+
+  # setter method
+  def initialize(brand)
+    @brand = brand
+  end
+
+  def color=(color)
+    @color = color
+  end
+
+  def size=(size)
+    @size = size
+  end
+
+  def material=(material)
+    @material = material
+  end
+
+  def condition=(condition)
+    @condition = condition
+  end
+
+  def cobble
+    @condition = "new"
+    puts "Your shoe is as good as new!" 
+  end
+
+
+end
